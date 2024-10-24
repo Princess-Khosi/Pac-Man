2de290a83134d4b70ecd186618d1f2fe608e288d
Princess <phlangu023@student.wethinkcode.co.za>
2024-10-24 08:59:05 +0200

    Commit

--- src/main/java/Training/Main.java
+++ src/main/java/Training/Main.java
@@ -1,7 +1,8 @@
 package Training;
+import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var app = Javalin.create().get("/", ctx -> ctx.result("Hello World")).start(7070);
     }
 }
