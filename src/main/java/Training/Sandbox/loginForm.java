267a64619199c9fc0419b75b0e7fb92dff35af0d
Princess Hlangu <phlangu023@stdent.wethinkcode.co.za>
2025-01-28 21:35:19 +0200

    Building momentum

---
+++ src/main/java/Training/Sandbox/loginForm.java
@@ -0,0 +1,48 @@
+package Training.Sandbox;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class loginForm extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,20));
+
+        Label userName = new Label("User Name");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
