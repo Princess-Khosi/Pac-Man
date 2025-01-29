2ae80fb5c5a9d91966fcd9edfde53a84d3dea47d
Princess Hlangu <phlangu023@stdent.wethinkcode.co.za>
2025-01-29 09:26:21 +0200

    The landing page of the game

--- src/main/java/Training/Sandbox/loginForm.java
+++ src/main/java/Training/Sandbox/loginForm.java
@@ -1,23 +1,29 @@
 package Training.Sandbox;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class loginForm extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("Welcome");
+
+        primaryStage.setTitle("Welcome!");
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -27,6 +33,7 @@ public class loginForm extends Application {
 
         Text scenetitle = new Text("Welcome");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,20));
+        grid.add(scenetitle,0,0,2,1);
 
         Label userName = new Label("User Name");
         grid.add(userName, 0, 1);
@@ -37,9 +44,33 @@ public class loginForm extends Application {
         Label password = new Label("Password:");
         grid.add(password, 0, 2);
 
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox,1,2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn,1,4);
+
+        final Text actiontarget = new Text();
+            grid.add(actiontarget,1,6);
+
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
+        primaryStage.show();
+
+        btn.setOnAction(new EventHandler<ActionEvent>(){
+
+        @Override
+        public void handle(ActionEvent e) {
+            actiontarget.setFill(Color.FIREBRICK);
+            actiontarget.setText("Sign in button pressed");
+        }
+        });
     }
+
+
 
     public static void main(String[] args) {
         launch();
